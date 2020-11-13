9cd1e81c6264b4cc0ca8618cbb50611ce13d9249
myvisualdna <57547185+myvisualdna@users.noreply.github.com>
2020-11-13 10:39:25 -0300

    sharing project

--- src/yourdata.js
+++ src/yourdata.js
@@ -1,56 +1,62 @@
-export default
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+  name: "Andres Nieves",
+  headerTagline: [
+    //Line 1 For Header
+    "Hi 👋 You found me! I am Andres Nieves.",
+    //Line 2 For Header
+    "Frontend Engineer ",
+    //Line 3 For Header
+    "from Florida, US",
+  ],
+  //Contact Email
+  contactEmail: "myvisualdna@gmail.com",
+  // Add Your About Text Here
+  abouttext:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  //Change This To Hide The Image of About Section (True Or False)
+  ShowAboutImage: true, //true or false (Change Here)
+  // Change Projects Here
+  projects: [
     {
-        //(Please Do Not Remove The comma(,) after every variable)
-        //Change The Website Template
-        name :'chetanverma.',
-        headerTagline: [//Line 1 For Header
-                        'Hi 👋 You found me! I am Your Name.',
-                        //Line 2 For Header
-                        'Digital designer ',
-                        //Line 3 For Header
-                        'from India'
-    ],
-        //Contact Email
-        contactEmail:'hello@chetanverma.com',
-        // Add Your About Text Here
-        abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-       //Change This To Hide The Image of About Section (True Or False)
-       ShowAboutImage:true,//true or false (Change Here)
-       // Change Projects Here 
-       projects:[
-           {
-            id: 1,//DO NOT CHANGE THIS (Please)😅
-            title:'Project One', //Project Title - Add Your Project Title Here
-             service:'UI/UX Design', // Add Your Service Type Here
-            //Project Image - Add Your Project Image Here
-             imageSrc:"https://images.unsplash.com/photo-1558104631-0fa41a8f6c20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-             //Project URL - Add Your Project Url Here
-             url:'http://chetanverma.com/'
-            },
-            {
-                id: 2,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Two',
-                service: 'Website Development',
-                imageSrc: "https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
-            },
-            { 
-                id: 3,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Three',
-                service: 'Web App',
-                imageSrc: "https://images.unsplash.com/photo-1511500118080-275313ec90a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
-            },
-            {
-                id: 4,//DO NOT CHANGE THIS (Please)😅
-                title: 'Project Four',
-                service: 'Branding',
-                imageSrc: "https://images.unsplash.com/photo-1558452919-08ae4aea8e29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'http://chetanverma.com/'
-           }
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Twitter Web Clone", //Project Title - Add Your Project Title Here
+      service: "Frontend & Backend Developed by Andres Nieves", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1596550190729-1d9225e788dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/myvisualdna/project-libra",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Whatsapp Clone",
+      service: "Frontend & Backend Developed by Andres Nieves",
+      imageSrc:
+        "https://images.unsplash.com/photo-1599382103240-5f2a57137d28?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80",
+      url: "https://github.com/myvisualdna/project-leo",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Amazon Web Clone",
+      service: "Frontend & Backend Developed by Andres Nieves",
+      imageSrc:
+        "https://images.unsplash.com/photo-1557899563-1940fc95709c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80",
+      url: "https://github.com/myvisualdna/project-aries",
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Weather App",
+      service: "Frontend and Backend WORK IN PROGRESS",
+      imageSrc:
+        "https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2195&q=80",
+      url: "https://github.com/myvisualdna/arietis",
+    },
 
-                    /*
+    /*
 
                     If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
                 ,{
@@ -61,23 +67,17 @@ export default
                 url: ''
             }
                 */
-        ],
-        social: [
-            // Add Or Remove The Link Accordingly
-            {   name:'Github',
-                url:'https://github.com/chetanverma16'},
-            {
-                name: 'Behance',
-                url: 'https://www.behance.net/chetanverma'
-            },
-            {
-                name: 'Dribbble',
-                url: 'https://dribbble.com/chetanverma'
-            },
-            {
-                name: 'Instagram',
-                url: 'https://www.instagram.com/cv.uidesign/'
-            }
-
-        ]
-    }
+  ],
+  social: [
+    // Add Or Remove The Link Accordingly
+    { name: "Github", url: "https://github.com/myvisualdna" },
+    {
+      name: "Behance",
+      url: "https://www.behance.net/uxadn",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/andres.nieves/",
+    },
+  ],
+};
